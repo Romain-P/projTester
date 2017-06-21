dd16983d3039996fbc37f9d9568f31215ab9b224
Romain <romain.pillot@epitech.eu>
2017-06-21 12:44:43 +0000

    Adding step one, show recursivly dirs

--- src/main.c
+++ src/main.c
@@ -5,13 +5,27 @@
 ** Login   <romain.pillot@epitech.net>
 ** 
 ** Started on  Thu Nov 24 11:14:29 2016 romain pillot
-** Last update Wed Jun 21 12:19:31 2017 romain pillot
+** Last update Wed Jun 21 12:43:58 2017 romain pillot
 */
 
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include "tester.h"
 #include "array.h"
+
+static void	show_dirs(t_node *node, int step)
+{
+  int		i;
+
+  i = -1;
+  while (++i < step * 5)
+    write(1, "-", 1);
+  printf("%s\n", file_getname(node->label));
+  i = -1;
+  while (++i < node->nodes->length)
+    show_dirs((t_node *)node->nodes->values[i], step + 1);
+}
 
 int		main(int ac, char **args)
 {
@@ -21,5 +35,7 @@ int		main(int ac, char **args)
   if (!(option = parse_options(ac, args)))
     return (_EXIT_FAILURE);
   node = parse_tree(args[1]);
+  if (!option->binary)
+    show_dirs(node, 0);
   return (_EXIT_SUCCESS);
 }
